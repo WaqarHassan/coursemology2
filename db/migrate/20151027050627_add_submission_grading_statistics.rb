35b66d27744eb3f7725e0f6dd8c2daab77408eb4
Aamir Mughal <waqar.hassan@devbox.co>
2017-07-01 10:05:11 +0500

    add_submittion grading statistics migration

--- db/migrate/20151027050627_add_submission_grading_statistics.rb
+++ db/migrate/20151027050627_add_submission_grading_statistics.rb
@@ -7,23 +7,23 @@ class AddSubmissionGradingStatistics < ActiveRecord::Migration
       t.datetime :graded_at
     end
 
-    root_user_id = exec_query('SELECT * FROM users').first['id']
-    exec_query('SELECT * FROM course_assessment_answers').each do |row|
-      if ['submitted', 'graded'].include?(row['workflow_state'])
-        execute(<<-SQL)
-          UPDATE course_assessment_answers
-          SET submitted_at=NOW(), grade=0
-          WHERE id=#{row['id']}
-        SQL
-      end
+    # root_user_id = exec_query('SELECT * FROM users').first['id']
+    # exec_query('SELECT * FROM course_assessment_answers').each do |row|
+    #   if ['submitted', 'graded'].include?(row['workflow_state'])
+    #     execute(<<-SQL)
+    #       UPDATE course_assessment_answers
+    #       SET submitted_at=NOW(), grade=0
+    #       WHERE id=#{row['id']}
+    #     SQL
+    #   end
 
-      next unless row['workflow_state'] == 'graded'
-      execute(<<-SQL)
-        UPDATE course_assessment_answers
-        SET graded_at=NOW(), grader_id=#{root_user_id}
-        WHERE id=#{row['id']}
-      SQL
-    end
+    #   next unless row['workflow_state'] == 'graded'
+    #   execute(<<-SQL)
+    #     UPDATE course_assessment_answers
+    #     SET graded_at=NOW(), grader_id=#{root_user_id}
+    #     WHERE id=#{row['id']}
+    #   SQL
+    # end
   end
 
   def down
