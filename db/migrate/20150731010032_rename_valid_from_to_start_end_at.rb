24833bbcdfd0559ef1a2d64d4543efeb0d0ceb7c
Aamir Mughal <waqar.hassan@devbox.co>
2017-07-01 10:01:10 +0500

    rename vaid from migration

--- db/migrate/20150731010032_rename_valid_from_to_start_end_at.rb
+++ db/migrate/20150731010032_rename_valid_from_to_start_end_at.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 class RenameValidFromToStartEndAt < ActiveRecord::Migration
   def change
-    rename_column :course_announcements, :valid_from, :start_at
-    rename_column :course_announcements, :valid_to, :end_at
-    rename_column :generic_announcements, :valid_from, :start_at
-    rename_column :generic_announcements, :valid_to, :end_at
+    # rename_column :course_announcements, :valid_from, :start_at
+    # rename_column :course_announcements, :valid_to, :end_at
+    # rename_column :generic_announcements, :valid_from, :start_at
+    # rename_column :generic_announcements, :valid_to, :end_at
   end
 end
